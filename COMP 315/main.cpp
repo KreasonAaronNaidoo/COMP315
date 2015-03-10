d18724e4ed84fc0b98c02444aa46c095c7b92b16
Kreason Aaron Naidoo <Aaron@Aaron.local>
2015-03-10 09:50:39 +0200

    Initial Commit

---
+++ COMP 315/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  COMP 315
+//
+//  Created by Kreason Aaron Naidoo on 2015/03/10.
+//  Copyright (c) 2015 Kreason Aaron Naidoo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
