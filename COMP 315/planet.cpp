242bd77be7cd9eb6b249e5aeea363344c5de8d72
Shaherin <shaherin22@gmail.com>
2015-03-30 20:00:37 +0200

    Update planet.cpp
    Added lighting and material to be removed upon finalization.
    Planet rotates at 1 degree per second. 
    Damage not finalized.

--- COMP 315/planet.cpp
+++ COMP 315/planet.cpp
@@ -6,10 +6,56 @@
 //  Copyright (c) 2015 Kreason Aaron Naidoo. All rights reserved.
 //
 
-#include "planet.h"
+#include <windows.h>
+#include <GL/glut.h>
 #include <iostream>
-#include <GLUT/glut.h>  // GLUT, include glu.h and gl.h
-#include <stdlib.h>
-#include <cmath>
-#include <stdio.h>
+#include <math.h>
+#include "planet.h"
+
+planet::planet(){
+   planet(0,0,0);
+}
+
+planet::planet(float x, float y, float z){
+    this->x = x;
+    this->y = y;
+    this->z = z;
+
+    angle=0.0;
+}
+
+void planet::render(){
+    glPushMatrix();
+    glTranslated(x, y, z); // move to this position
+
+    rotate();
+
+    // sets specular properties of the material
+    GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+    glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+
+    // sets the shininess of the material
+    GLfloat mat_shininess[] = { 50.0 };
+    glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
+
+    glutSolidSphere(0.5,25,25);//radius 0.5, 25 slices and stacks
+    //glutWireSphere(0.5,25,25);
+    glPopMatrix();
+}
+
+void planet::rotate(){
+   //replace with angular velocity?
+   angle+=1.0f;
+
+    if(angle>360.f)
+    {
+      angle-=360;
+    }
+
+    glRotatef(angle,-0.2,1,0);
+}
+
+void takeDamage(){
+   //subtract an amount dependent on asteroid size from main health if collision is detected
+}
 
