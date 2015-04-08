187953a2b418e636d2defef63f563629da794b6a
bassamuhammad <bassamuhammad@gmail.com>
2015-04-08 21:10:23 +0200

    Update player.cpp

--- COMP 315/player.cpp
+++ COMP 315/player.cpp
@@ -1,13 +1,50 @@
-//
-//  player.cpp
-//  COMP 315
-//
-//  Created by Kreason Aaron Naidoo on 2015/03/24.
-//  Copyright (c) 2015 Kreason Aaron Naidoo. All rights reserved.
-//
-
 #include "player.h"
+#include <windows.h>
+#include <GL/glut.h>
+#include <iostream>
+#include<math.h>
+
+
+
 
 player::player(){
-    
+
+}
+
+player::player(float x, float y, float z){
+    this->x = x;
+    this->y = y;
+    this->z = z;
+}
+
+
+void player::render(){
+
+    glPushMatrix();
+    glTranslatef(x, y, z); // move to this position
+    drawplayer();
+    glPopMatrix();
+}
+
+void player::drawplayer(){
+    glScalef(0.1f,0.1f,0.1f);
+	glTranslatef(0.0f,1.0f,0.0f);
+    glBegin(GL_TRIANGLES);
+        glColor3d(255,0,255);
+		glVertex3f( 0.0f, 1.0f, 0.0f);
+		glVertex3f(-1.0f,-1.0f, 1.0f);
+		glVertex3f( 1.0f,-1.0f, 1.0f);
+		glColor3d(0,255,0);
+		glVertex3f( 0.0f, 1.0f, 0.0f);
+		glVertex3f( 1.0f,-1.0f, 1.0f);
+		glVertex3f( 1.0f,-1.0f, -1.0f);
+		glColor3d(255,255,0);
+		glVertex3f( 0.0f, 1.0f, 0.0f);
+		glVertex3f( 1.0f,-1.0f, -1.0f);
+		glVertex3f(-1.0f,-1.0f, -1.0f);
+		glColor3d(0,0,255);
+		glVertex3f( 0.0f, 1.0f, 0.0f);
+		glVertex3f(-1.0f,-1.0f,-1.0f);
+		glVertex3f(-1.0f,-1.0f, 1.0f);
+	glEnd();
 }
