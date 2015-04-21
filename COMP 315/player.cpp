7e8ab4fa4b947d24e3836f7d4f8d3ac3e681ccf1
Shaherin <shaherin22@gmail.com>
2015-04-21 21:31:40 +0200

    Update player.cpp

--- COMP 315/player.cpp
+++ COMP 315/player.cpp
@@ -1,34 +1,29 @@
 #include "player.h"
-#include <GLUT/glut.h>
+#include <GL/glut.h>
 #include <iostream>
 #include<math.h>
 #include "npc.h"
 #include "point.h"
 
-using namespace std;
-
-
-
 player::player(){
 
 }
 
-player::player(float x, float y, float z, cam *P){
+player::player(float x, float y, float z){
     this->x = x;
     this->y = y;
     this->z = z;
-    q = P;
-    
 }
 
 void player::shoot(){
-    //mag.push_back(new bullet(x,y,z,0,0,500.0)); // change to mouse position
-    //mag.push_back(new bullet(x,y,z,0,10,5));//player position to mouse position
-   
-    
-    
-    mag.push_back(new bullet(x,y,z,1,1,1000));
-    //cout<<"true";
+    //cout<<"click"<<endl;
+    v_bullet.push_back(*new bullet(this->x,this->y,this->z,x,y,0));//player position to mouse position
+
+    /*POINT pt;
+    bool result=GetCursorPos(&pt);
+    cout<<pt.x<<endl;
+    mag.push_back(new bullet(x,y,z,pt.x-650,pt.y-400,50));*/
+
 }
 
 void player::render(){
@@ -44,8 +39,8 @@ void player::render(){
 }
 
 void player::drawBullets(){
-    for(int i=0 ; i<mag.size() ; i++){
-        mag[i].render();
+    for(int i=0 ; i<v_bullet.size() ; i++){
+        v_bullet[i].render();
     }
 }
 
@@ -75,6 +70,6 @@ void collisionCheck(){
 
 }
 
-vector<bullet>* player::getBulletVector(){
-    return &mag;
+vector<bullet> player::getBulletVector(){
+    return v_bullet;
 }
